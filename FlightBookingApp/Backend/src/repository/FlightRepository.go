0b9564fe18ccebf442c5d2497f77f2563996167b
stojanovic00 <al.stojanovic0@gmail.com>
2023-03-22 13:54:32 +0100

    Repository refactor
    Generalized connection logic

--- FlightBookingApp/Backend/src/repository/FlightRepository.go
+++ FlightBookingApp/Backend/src/repository/FlightRepository.go
@@ -4,22 +4,14 @@ import (
 	"FlightBookingApp/errors"
 	"FlightBookingApp/model"
 	"context"
-	"fmt"
 	"github.com/google/uuid"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
-	"os"
-	"time"
 )
 
 type flightRepository struct {
+	base    Repository
 	Flights []model.Flight
-	cli     *mongo.Client
-	logger  *log.Logger
 }
 
 type FlightRepository interface {
@@ -31,71 +23,42 @@ type FlightRepository interface {
 
 // NoSQL: Constructor which reads db configuration from environment
 func NewFlightRepository(ctx context.Context, logger *log.Logger) (*flightRepository, error) {
-	dburi := os.Getenv("MONGO_DB_URI")
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(dburi))
+	client, err := GetClient()
 	if err != nil {
 		return nil, err
 	}
-
-	err = client.Connect(ctx)
-	if err != nil {
-		return nil, err
-	}
-	logger.Println("Successful connection")
-
 	return &flightRepository{
-		cli:    client,
-		logger: logger,
+		base: Repository{
+			client: client,
+			logger: logger,
+		},
 	}, nil
-
 }
 
-// Disconnect from database
-func (pr *flightRepository) Disconnect(ctx context.Context) error {
-	err := pr.cli.Disconnect(ctx)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Check database connection
-func (repo *flightRepository) Ping() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	// Check connection -> if no error, connection is established
-	err := repo.cli.Ping(ctx, readpref.Primary())
-	if err != nil {
-		repo.logger.Println(err)
-	}
-
-	// Print available databases
-	databases, err := repo.cli.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		repo.logger.Println(err)
-	}
-	fmt.Println(databases)
-}
-
-func (repository *flightRepository) Create(flight model.Flight) model.Flight {
-	repository.Flights = append(repository.Flights, flight)
+// TODO Aleksandar: napraviti connection pooling da ne bi morale konstantno da se otvaraju i zatvaraju konekcije sa bazom
+func (repo *flightRepository) Create(flight model.Flight) model.Flight {
+	repo.base.Connect(context.Background())
+	defer repo.base.Disconnect(context.Background())
+	repo.Flights = append(repo.Flights, flight)
 	return flight
 }
 
-func (repository *flightRepository) GetAll() []model.Flight {
-	return repository.Flights
+func (repo *flightRepository) GetAll() []model.Flight {
+	repo.base.Connect(context.Background())
+	defer repo.base.Disconnect(context.Background())
+	return repo.Flights
 }
 
-func (repository *flightRepository) GetById(id uuid.UUID) (model.Flight, error) {
-	for _, flight := range repository.Flights {
+func (repo *flightRepository) GetById(id uuid.UUID) (model.Flight, error) {
+	repo.base.Connect(context.Background())
+	defer repo.base.Disconnect(context.Background())
+	for _, flight := range repo.Flights {
 		if flight.ID == id {
 			return flight, nil
 		}
 	}
 	return model.Flight{}, &errors.NotFoundError{}
 }
-func (repository *flightRepository) Delete(id primitive.ObjectID) error {
+func (repo *flightRepository) Delete(id primitive.ObjectID) error {
 	return nil
 }
