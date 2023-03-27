5bea792a99dd11b052aeb924a9dfdcc27c322bc5
Your Name <srdanov.ra145.2019@uns.ac.rs>
2023-03-27 21:39:21 +0200

    Flight search spike

---
+++ FlightBookingApp/frontend-flight/src/components/flight-search/flight-search.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function FlightSearch() {
+    return (
+        <div style={{height: 400, width: '100%'}}>
+            <h1>Ovde promeni</h1>
+        </div>
+    );
+}
--- FlightBookingApp/frontend-flight/src/pages/customer/flight-search-page.js
+++ FlightBookingApp/frontend-flight/src/pages/customer/flight-search-page.js
@@ -1,7 +1,12 @@
+import FlightSearch from "../../components/flight-search/flight-search";
+
 function FlightSearchPage() {
-  return (
-    <div><h1>Flight Search</h1></div>
-  )
+    return (
+        <div>
+            <h1>Flight Search</h1>
+            <FlightSearch></FlightSearch>
+        </div>
+    )
 }
 
 export default FlightSearchPage
