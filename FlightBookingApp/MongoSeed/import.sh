274296b0cf7f30946e496a1c6d0d57c7a658c8ad
Your Name <srdanov.ra145.2019@uns.ac.rs>
2023-04-01 03:57:12 +0200

    Fixed endpoints for admin role

--- FlightBookingApp/MongoSeed/import.sh
+++ FlightBookingApp/MongoSeed/import.sh
@@ -1,3 +1,6 @@
+echo "DATABASE INIT START #############################################";
 mongoimport --uri "mongodb://root:pass@mongo:27017" --db flightDb --collection airports  --file json/airports.json --authenticationDatabase=admin  --jsonArray --drop;
-mongoimport --uri "mongodb://root:pass@mongo:27017"  --db flightDb --collection users --file json/users.json --authenticationDatabase=admin  --jsonArray --drop;
-mongoimport --uri "mongodb://root:pass@mongo:27017"   --db flightDb --collection accounts --file json/accounts.json --authenticationDatabase=admin  --jsonArray --drop;
+mongoimport --uri "mongodb://root:pass@mongo:27017" --db flightDb --collection users --file json/users.json --authenticationDatabase=admin  --jsonArray --drop;
+mongoimport --uri "mongodb://root:pass@mongo:27017" --db flightDb --collection accounts --file json/accounts.json --authenticationDatabase=admin  --jsonArray --drop;
+mongoimport --uri "mongodb://root:pass@mongo:27017" --db flightDb --collection flights --file json/flights.json --authenticationDatabase=admin  --jsonArray --drop;
+echo "DATABASE INIT END #############################################";
