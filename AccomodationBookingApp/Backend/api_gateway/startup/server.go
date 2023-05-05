4a8999977dfcab723d8e8b82d8baab779cfbbb64
stefanapostolovic <93873256+stefanapostolovic@users.noreply.github.com>
2023-05-05 23:27:19 +0200

    api gateway middleware implementation started

--- AccomodationBookingApp/Backend/api_gateway/startup/server.go
+++ AccomodationBookingApp/Backend/api_gateway/startup/server.go
@@ -13,28 +13,46 @@ import (
 )
 
 type Server struct {
-	config *configuration.Configuration
-	mux    *runtime.ServeMux
+	config  *configuration.Configuration
+	handler *http.Handler
 }
 
 func NewServer(config *configuration.Configuration) *Server {
+	mux := runtime.NewServeMux()
+	handler := createAuthTokenMiddleware(mux)
+
 	server := &Server{
-		config: config,
-		mux:    runtime.NewServeMux(),
+		config:  config,
+		handler: &handler,
 	}
 	server.initHandlers()
+	// custom handlers with auth
+
 	return server
+}
+
+func createAuthTokenMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header missing", http.StatusUnauthorized)
+			return
+		}
+		accessToken := authHeader[len("Bearer "):]
+		ctx := context.WithValue(r.Context(), "access_token", accessToken)
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 func (server *Server) initHandlers() {
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 	authorizationEndpoint := fmt.Sprintf("%s:%s", server.config.AuthorizationHost, server.config.AuthorizationPort)
-	err := authorization.RegisterAuthorizationServiceHandlerFromEndpoint(context.TODO(), server.mux, authorizationEndpoint, opts)
+	err := authorization.RegisterAuthorizationServiceHandlerFromEndpoint(context.TODO(), server.handler, authorizationEndpoint, opts)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func (server *Server) Start() {
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", server.config.Port), server.mux))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", server.config.Port), *server.handler))
 }
