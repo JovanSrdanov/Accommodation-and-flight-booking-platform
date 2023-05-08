09a3b4c2e197545203b65ab300772ca0387ce7d9
strale15 <karte32114@gmail.com>
2023-05-09 01:21:27 +0200

    gascina

--- AccomodationBookingApp/Backend/accommodation_service/persistence/repository/AccommodationRepositoryMongo.go
+++ AccomodationBookingApp/Backend/accommodation_service/persistence/repository/AccommodationRepositoryMongo.go
@@ -2,9 +2,11 @@ package repository
 
 import (
 	"accommodation_service/domain/model"
-	"fmt"
+	"context"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"time"
 )
 
 type AccommodationRepositoryMongo struct {
@@ -16,9 +18,25 @@ func NewAccommodationRepositoryMongo(dbClient *mongo.Client) (*AccommodationRepo
 }
 
 func (repo AccommodationRepositoryMongo) Create(accommodation *model.Accommodation) (primitive.ObjectID, error) {
-	fmt.Println(accommodation.Name + " TEST*********")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	collection := repo.getCollection()
+
+	log.Println("UZEO COLLECTION")
+
 	accommodation.ID = primitive.NewObjectID()
-	return accommodation.ID, nil
+
+	log.Println(accommodation)
+
+	result, err := collection.InsertOne(ctx, &accommodation)
+	if err != nil {
+		log.Println(err)
+		return primitive.ObjectID{}, err
+	}
+	id := result.InsertedID.(primitive.ObjectID)
+	log.Println("Inserted entity, id = '%s'\n", id)
+	return id, nil
 }
 
 func (repo AccommodationRepositoryMongo) Delete(id primitive.ObjectID) error {
@@ -31,4 +49,10 @@ func (repo AccommodationRepositoryMongo) Update(accommodation *model.Accommodati
 
 func (repo AccommodationRepositoryMongo) GetById(id primitive.ObjectID) (*model.Accommodation, error) {
 	return &model.Accommodation{}, nil
+}
+
+func (repo AccommodationRepositoryMongo) getCollection() *mongo.Collection {
+	db := repo.dbClient.Database("accommodationDb")
+	collection := db.Collection("accommodations")
+	return collection
 }
--- AccomodationBookingApp/Backend/accommodation_service/persistence/repository/Client.go
+++ AccomodationBookingApp/Backend/accommodation_service/persistence/repository/Client.go
@@ -1,17 +1,20 @@
 package repository
 
 import (
+	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
 
 func GetClient(user string, pass string) (*mongo.Client, error) {
-	dbUri := "mongodb://" + user + ":" + pass + "@mongo:27017"
+	dbUri := "mongodb://mongo:27017"
+	log.Println(dbUri + " ALOOOO***")
 
 	clientOptions := options.Client().ApplyURI(dbUri)
-	client, err := mongo.NewClient(clientOptions)
-
+	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
+		log.Println(err)
 		return nil, err
 	}
 
